9293c35143943c4a6d25181582c2dac902698bf0
chetanhpatel <imp.chetanpatel@gmail.com>
2018-10-27 17:29:12 +0530

    gutil log first comment

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,7 @@ gulp.task('copy', function() {
     gulp.src('index.html')
     .pipe(gulp.dest('assets'))
 });
+var gutil = require('gulp-util');
+gulp.task('log',function(){
+    gutil.log('==my first log==');
+})
